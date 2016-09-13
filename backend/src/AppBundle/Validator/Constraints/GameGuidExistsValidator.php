8f421cf174c9ccc34db4bf2e505fd6a012a6ee68
Ricardo Velhote <rvelhote@gmail.com>
2016-09-13 19:53:43 +0100

    ADD: Validator to check if the GUID passed as a parameter exists and is valid

--- backend/src/AppBundle/Validator/Constraints/GameGuidExistsValidator.php
+++ backend/src/AppBundle/Validator/Constraints/GameGuidExistsValidator.php
@@ -1,23 +1,41 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: rvelhote
- * Date: 9/12/16
- * Time: 10:13 PM
+ * MIT License
+ *
+ * Copyright (c) 2016 Ricardo Velhote
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
-
 namespace AppBundle\Validator\Constraints;
 
-
 use AppBundle\Repository\GameRepository;
-use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
+/**
+ * Class GameGuidExistsValidator
+ * @package AppBundle\Validator\Constraints
+ */
 class GameGuidExistsValidator extends ConstraintValidator
 {
     /**
-     * @var GameRepository
+     * @var GameRepository Doctrine repository to access the database.
      */
     private $repository;
 
@@ -32,16 +50,21 @@ class GameGuidExistsValidator extends ConstraintValidator
 
     /**
      * Checks if the passed value is valid.
+     * Verify that the game GUID exists in the database and is playable.
      *
-     * @param mixed $value The value that should be validated
-     * @param Constraint $constraint The constraint for the validation
+     * @param mixed $value The value that should be validated.
+     * @param Constraint $constraint The constraint for the validation.
+     *
      * @throws \Exception
      */
     public function validate($value, Constraint $constraint)
     {
-        $game = $this->repository->findOneBy(['guid' => "xxx".$value]);
-
-        $this->context->buildViolation($constraint->message)->setParameter('%string%', $value)->addViolation();
-
+        $game = $this->repository->findOneBy(['guid' => $value]);
+        if (is_null($game)) {
+            $this->context
+                ->buildViolation($constraint->message)
+                ->setParameter('%guid%', $value)
+                ->addViolation();
+        }
     }
 }
