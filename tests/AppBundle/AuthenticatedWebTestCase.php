85ceba932fa9c854deb7eba774ceed53a00cb84f
Ricardo Velhote <rvelhote@gmail.com>
2016-10-26 20:07:49 +0100

    ADD: Create a class that will allow us to create an authenticated client with a JWT token

---
+++ tests/AppBundle/AuthenticatedWebTestCase.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * MIT License
+ *
+ * Copyright (c) 2016 Ricardo Velhote
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+namespace Tests\AppBundle;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Class AuthenticatedWebTestCase
+ * @package Tests\AppBundle
+ */
+class AuthenticatedWebTestCase extends WebTestCase
+{
+    /**
+     * Create a client with a default Authorization header.
+     *
+     * @param string $username
+     * @param string $password
+     *
+     * @return \Symfony\Bundle\FrameworkBundle\Client
+     */
+    protected function createAuthenticatedClient($username = '@rvelhote', $password = 'x')
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/api/v1/login',
+            array(
+                '_username' => $username,
+                '_password' => $password,
+            )
+        );
+
+        $data = json_decode($client->getResponse()->getContent(), true);
+
+        $client = static::createClient();
+        $client->setServerParameter('HTTP_Authorization', sprintf('Bearer %s', $data['token']));
+
+        return $client;
+    }
+}
